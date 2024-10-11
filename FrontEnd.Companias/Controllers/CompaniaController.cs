20a4f876bc276d4b48d4524e039fcbadc58d22fe
Luis <Luis@DESKTOP-6NG7IJ3>
2024-10-10 18:16:48 -0600

    Agregar archivos de proyecto.

---
+++ FrontEnd.Companias/Controllers/CompaniaController.cs
@@ -0,0 +1,88 @@
+﻿using FrontEnd.Companias.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Companias.Controllers
+{
+    public class CompaniaController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public CompaniaController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> ObtenerCompania()
+        {
+            List<Compania> listaCompania = new List<Compania>();
+            string UrlBusqueda = configuration["UrlApiBusqueda"];
+            try
+            {
+                // Se onsume Api para obtener compañias
+                var httpClient = new HttpClient();
+                var json = await httpClient.GetStringAsync(UrlBusqueda);
+                listaCompania = JsonConvert.DeserializeObject<List<Compania>>(json);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+            return View(listaCompania);
+        }
+
+        public ActionResult GuardarCompania()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> GuardarCompania(Compania compania)
+        {
+            HttpResponseMessage result = new HttpResponseMessage();
+            string UrlGuardar = configuration["UrlApiGuardar"];
+            try
+            {
+                //Validamos que los campos no esten vacios
+                if (ModelState.IsValid)
+                {
+                    //Se consume Api para Guardar Registro Compania
+                    var httpClient = new HttpClient();
+                     result = await httpClient.PostAsJsonAsync<Compania>(UrlGuardar, compania);
+                    ViewBag.IsSuccessStatusCode = result.IsSuccessStatusCode.ToString();
+                }
+                if (result.IsSuccessStatusCode)
+                {
+                    ViewBag.NombreCompañia = compania.NombreCompania;
+                    ViewBag.NombreContacto = compania.NombreContacto;
+                    ViewBag.CorreoElectronico = compania.CorreoElectronico;
+                    ViewBag.Telefono = compania.Telefono;                    
+                }
+                else
+                {
+                    ViewBag.Mensaje = "No se pudo guardar el registro";
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return View(ex.Message.ToString());
+            }
+            
+
+            return View();
+        }
+    }
+}
---
+++ FrontEnd.Companias/Models/Compania.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Companias.Models
+{
+    public class Compania
+    {
+        [Display(Name = "Compañia")]
+        [Required(ErrorMessage = "La  Compañia es obligatorio")]
+        public string NombreCompania { get; set; }
+
+        [Display(Name = "Contacto")]
+        [Required(ErrorMessage = "El Contacto es obligatorio")]
+        public string NombreContacto { get; set; }
+
+        [Display(Name = "Correo")]
+        [Required(ErrorMessage = "El Correo es obligatorio")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "El correo no es valido")]
+        public string CorreoElectronico { get; set; }
+
+        [Display(Name = "Telefono")]
+        [Required(ErrorMessage = "El Telefono es obligatorio")]
+        [RegularExpression("(^[0-9]+$)", ErrorMessage = "Solo se permiten números")]
+        public string Telefono { get; set; }
+
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Seleccione el aviso de privacidad.")]
+        public bool Aviso { get; set; }
+
+    }
+}
